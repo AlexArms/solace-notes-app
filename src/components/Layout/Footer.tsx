035d1fa4fe9035d913ca4ce57115efc38ead971e
Alex <alexaarms96@gmail.com>
2023-11-02 14:23:09 -0600

    added basic contact info to footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -7,12 +7,21 @@ const Footer = () => {
         width: "100%",
         textAlign: "center",
         fontSize: "2rem",
+        display: "flex",
         position: "sticky",
         bottom: 0,
+        justifyContent: "center",
+        gap: "50px",
         marginBottom: "50px",
       }}
     >
-      Contact
+      <a href="mailto: alexaarms96@gmail.com">Email Me</a>
+      <a
+        href="https://github.com/AlexArms?tab=repositories"
+        target="_blank"
+      >
+        My Github
+      </a>
     </div>
   );
 };
