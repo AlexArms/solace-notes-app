edb23cd216ffb74a7bd57473521e36f291a35de0
Alex <alexaarms96@gmail.com>
2023-11-03 11:40:38 -0600

    removed .env file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,5 @@
-import { Inter } from "next/font/google";
 import NoteFeed from "@/components/Notes/NoteFeed";
 import { useUserStore } from "@/stores/UserStore";
-import { GetServerSideProps } from "next";
-
-const inter = Inter({ subsets: ["latin"] });
-
-type ConnectionStatus = {
-  isConnected: boolean;
-};
-
-export const getServerSideProps: GetServerSideProps<
-  ConnectionStatus
-> = async () => {
-  try {
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
-};
 
 export default function Home() {
   const userStore = useUserStore();
